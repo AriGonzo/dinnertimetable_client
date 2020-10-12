51bb39e2a9671f5e9a2afc8f80e19b421614284e
Ari Gonzalez <arigonzalez515@gmail.com>
2020-10-12 15:20:42 -0400

    Initial commit

---
+++ src/app/modals/add-new-meal/add-new-meal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-new-meal',
+  templateUrl: './add-new-meal.component.html',
+  styleUrls: ['./add-new-meal.component.scss']
+})
+export class AddNewMealComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
